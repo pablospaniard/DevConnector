ba68f4ef2d2e7e0d1177c90ca079ee57816b1288
Pavel Ivanov <pablospaniard@gmail.com>
2018-07-18 10:23:11 +0200

    settings tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,7 @@ module.exports = {
         '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
             'eslint --fix',
             'prettier --write',
+            'yarn test',
             'git add',
         ],
     },
