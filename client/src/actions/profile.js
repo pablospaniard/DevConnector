5bfa3f62f960d80a880394f2b823d0302b951af2
Pavel Ivanov <pablospaniard@gmail.com>
2018-07-19 15:26:00 +0200

    create profile done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,31 +3,43 @@ import axios from 'axios'
 import * as constants from './constants'
 
 export const getCurrentProfile = () => dispatch => {
-    dispatch(setProfileLoading())
-    axios
-        .get('/api/profile')
-        .then(res =>
-            dispatch({
-                type: constants.GET_PROFILE,
-                payload: res.data,
-            }),
-        )
-        .catch(err =>
-            dispatch({
-                type: constants.GET_PROFILE,
-                payload: {},
-            }),
-        )
+  dispatch(setProfileLoading())
+  axios
+    .get('/api/profile')
+    .then(res =>
+      dispatch({
+        type: constants.GET_PROFILE,
+        payload: res.data,
+      }),
+    )
+    .catch(err =>
+      dispatch({
+        type: constants.GET_PROFILE,
+        payload: {},
+      }),
+    )
+}
+
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: constants.GET_ERRORS,
+        payload: err.response.data,
+      }),
+    )
 }
 
 export const setProfileLoading = () => {
-    return {
-        type: constants.PROFILE_LOADING,
-    }
+  return {
+    type: constants.PROFILE_LOADING,
+  }
 }
 
 export const clearCurrentProfile = () => {
-    return {
-        type: constants.CLEAR_CURRENT_PROFILE,
-    }
+  return {
+    type: constants.CLEAR_CURRENT_PROFILE,
+  }
 }
